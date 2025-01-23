dc0f64838027075dfb0e4cf36dda996fcfe69a6b
Elias Zambrano <adrianzambranomontero@gmail.com>
2025-01-23 21:50:13 +0100

    registro_terminado

--- app/src/main/java/com/finalproyect/medallium/LoginActivity.java
+++ app/src/main/java/com/finalproyect/medallium/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.finalproyect.medallium;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,10 +19,19 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        TextView registerClickable = findViewById(R.id.register_clickable);
+
+        registerClickable.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchRegister();
+
+            }
         });
+    }
+    public void launchRegister() {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 }
