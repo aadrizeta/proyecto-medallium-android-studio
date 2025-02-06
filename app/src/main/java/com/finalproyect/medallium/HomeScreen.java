ee8f9e99e71d931e1e83f30d7a413dd1faa59a00
Antonio <antonio_ajb@outlook.com>
2025-02-07 00:47:00 +0100

    Haciendo la view del Home.

--- app/src/main/java/com/finalproyect/medallium/HomeScreen.java
+++ app/src/main/java/com/finalproyect/medallium/HomeScreen.java
@@ -3,37 +3,34 @@ package com.finalproyect.medallium;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class RegisterActivity extends AppCompatActivity {
+public class HomeScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_home_screen);
 
-        TextView loginClickable = findViewById(R.id.login_clickable);
+        CardView cardViewLogo = findViewById(R.id.cardViewLogo);
 
-        loginClickable.setOnClickListener(new View.OnClickListener() {
+        cardViewLogo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                launchLogin();
+                launchProfile();
             }
         });
-
     }
 
-    public void launchLogin() {
-        Intent intent = new Intent(this, LoginActivity.class);
+    public void launchProfile() {
+        Intent intent = new Intent(this, Profile.class);
         startActivity(intent);
     }
-
 }
-
